38ab42e8a7634a86a778f2a10c0fec12e64430d1
Ales Zoulek <ales.zoulek@gmail.com>
2018-01-28 12:33:51 +0100

    Production db settings

--- wt_accounts/settings/production_base.py
+++ wt_accounts/settings/production_base.py
@@ -3,3 +3,15 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOST = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wt',
+        'USER': 'wt',
+        'PASSWORD': '',
+        'HOST': 'wt-accounts.cvlintg75rfu.ap-northeast-1.rds.amazonaws.com',
+        'OPTIONS': {'charset': 'utf8mb4'},
+    }
+}
+
