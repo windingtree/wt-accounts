df43748692cf343686307f18b8987318354de0f3
AugustoL <augusto.lemble@gmail.com>
2018-02-01 03:19:58 -0300

    Show only verification items if contributions is higher than 10ETH

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ var SOFT_CAP = 4350;
 var icoAddress = "0x9df3a24d738ae98dea766cd89c3aef16583a4daf";
 var tokenAddress = "0xeb9951021698b42e4399f9cbb6267aa35f82d59d";
 var contributorAddress = userAddress;
-var maxUnverifiedContribution = 15;
+var maxUnverifiedContribution = 10;
 
 //Set addresses
 $('#icoAddress').text(icoAddress);
@@ -67,12 +67,14 @@ function refreshUserContribution() {
       getEthSent(contributorAddress).then(function(weiSent) {
         var ETHSent = (Number(weiSent.result) / 1e18);
         console.log('ETH Sent:', ETHSent);
-        // if (ETHSent > maxUnverifiedContribution && verifiedUser) {
-        //   $('#verify-profile').show();
-        //   $('#verify-alert').show();
-        // }
-        $('#verify-profile').show();
-        $('#verify-alert').show();
+
+        // Show only profile verification if contribution more than maxUnverifiedContribution
+        if (ETHSent >= maxUnverifiedContribution && verifiedUser) {
+          $('#verification-status').show();
+          $('#proof-of-address-input').show();
+          $('#verify-profile').show();
+          $('#verify-alert').show();
+        }
         $('#totalETHSent').text(parseFloat(ETHSent).toFixed(4)+' ETH');
       });
     else
